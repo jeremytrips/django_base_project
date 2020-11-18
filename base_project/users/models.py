6c1a0be3c136fd401d0ccfe2474c996e6e12aa6c
Vincent FISCHER <17325@ecam.be>
2020-11-18 12:56:47 +0100

    Ajout des default dans models.py

--- base_project/users/models.py
+++ base_project/users/models.py
@@ -9,18 +9,18 @@ from .other_models.settings import Settings
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)                                                  # Adresse mail pour contacter et se connecter
     noma = models.CharField(max_length=10, default = "NoNoma")                              # Numero de matricule de l etudiant(e)
-    student_card = models.ImageField(default = ".\static\no_img.png")                       # Carte etudiant pour verif mail/noma/nom et validite du compte
+    student_card = models.ImageField(default = "\static\no_img.png")                        # Carte etudiant pour verif mail/noma/nom et validite du compte
     name = models.CharField(max_length=20, default = "NoName")                              # prenom de l etudiant(e)
     surname = models.CharField(max_length=20, default="NoSurname")                          # nom de l etudiant(e)
     studies = models.CharField(max_length = 30, default = "NoStudies")                      # etudes de l etudiant(e)
     home_adress = models.CharField(max_length=30, default = "NoHomeAdd")                    # lieu de residence hors campus
     birth_date = models.DateTimeField(default = "NoBirth")                                  # date de naissance de l etudiant(e)
-    description = models.CharField(max_length=250, default=" ")                             # description visible pour les autres utilisateurs
-    account_is_visible = models.BooleanField()                                              # le compte est visible ou inactif
-    interest_age_fork = InclusiveIntegerRangeField()                                        # fourchette d age d interet pour partenaire
-    profile_pic_one = models.ImageField(blank=True, default = ".\static\no_img.png")            # premiere image de l'utilisateur
-    profile_pic_two = models.ImageField(blank=True, default = ".\static\no_img.png")            # deuxieme image de l'utilisateur
-    profile_pic_three = models.ImageField(blank=True, default = ".\static\no_img.png")          # troisieme image de l'utilisateur
+    description = models.CharField(max_length=250, default="")                              # description visible pour les autres utilisateurs
+    age_lower_bound = models.PositiveSmallIntegerField(default=16)                          # limite inf d age pour partenaire
+    age_upper_bound = models.PositiveSmallIntegerField(default=100)                         # limite sup d age pour partenaire
+    profile_pic_one = models.ImageField(blank=True, default = "\static\no_img.png")            # premiere image de l'utilisateur
+    profile_pic_two = models.ImageField(blank=True, default = "\static\no_img.png")            # deuxieme image de l'utilisateur
+    profile_pic_three = models.ImageField(blank=True, default = "\static\no_img.png")          # troisieme image de l'utilisateur
     settings = models.OneToOneField(Settings, on_delete=models.CASCADE)
 
     objects = CustomUserManager()
--- base_project/users/other_models/settings.py
+++ base_project/users/other_models/settings.py
@@ -7,8 +7,9 @@ class Settings(models.Model):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     email_verified = models.BooleanField(default=False)
+    account_is_visible = models.BooleanField(default = True)                             
 
-    push_notification_new_relation = models.BooleanField(default=True)                  # s explique de par le nom
+    push_notification_new_relation = models.BooleanField(default=True)               
     mail_notification_new_relation = models.BooleanField(default=True)
     push_notification_new_message = models.BooleanField(default=True)
     mail_notification_new_message = models.BooleanField(default=True)
