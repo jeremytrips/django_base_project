d40b1ca2d1e85d4eba7fb097c22674475ec4f7d3
jeremytrips <51384000+jeremytrips@users.noreply.github.com>
2020-11-18 22:31:28 +0100

    Add authentication urls

---
+++ base_project/users/api/urls.py
@@ -0,0 +1,28 @@
+"""Didit URL Configuration
+
+The `urlpatterns` list routes URLs to classview. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function classview
+    1. Add an import:  from my_app import classview
+    2. Add a URL to urlpatterns:  path('', classview.home, name='home')
+Class-based classview
+    1. Add an import:  from other_app.classview import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
+
+from .views.authentication import LoginVIew, LogoutView
+from .views.registrationview import RegistrationView
+
+urlpatterns = [
+    path('create/', RegistrationView.as_view(), name='create'),
+    # path('delete/', Delete.as_view(), name='delete'),
+    path('login/', LoginVIew.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
