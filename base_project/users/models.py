2f05c5d9d5378513b91135e39e6516e6a85ebf3f
Vincent FISCHER <17325@ecam.be>
2020-11-18 11:12:52 +0100

    Legeres corrections

--- base_project/users/models.py
+++ base_project/users/models.py
@@ -9,11 +9,11 @@ class CustomUser(AbstractUser):
     mail = models.EmailField()                                  #Adresse mail pour contacter et se connecter
     noma = models.CharField(max_length = 10)                    #Numero de matricule de l etudiant(e)
     student_card = models.ImageField()                          #Carte etudiant pour verif mail/noma/nom et validite du compte
-    name = models.CharField(max_length = 20)                    #prenom de l etudiant(e)
-    surname = models.CharField(max_length = 20)                 #nom de l etudiant(e)
+    first_name = models.CharField(max_length = 20)                    #prenom de l etudiant(e)
+    last_name = models.CharField(max_length = 20)                 #nom de l etudiant(e)
     studies = models.CharField(max_length = 30)                 #etudes de l etudiant(e)
-    home_adress = models.CharField(max_length = 30)             #lieu de residence hors campus
-    birt_date = models.DateTimeField()                          #date de naissance de l etudiant(e)
+    hometown = models.CharField(max_length = 30)             #lieu de residence hors campus
+    birth_date = models.DateTimeField()                          #date de naissance de l etudiant(e)
     description = models.CharField(max_length = 250)            #description visible pour les autres utilisateurs
     account_is_visible = models.BooleanField()                  #le compte est visible ou inactif
     interest_age_fork = InclusiveIntegerRangeField()            #fourchette d age d interet pour partenaire
